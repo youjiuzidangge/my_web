4067c53185df08e640d464b3d78df801c7cf4417
xin.li <lixin@kid17.com>
2019-08-27 11:09:52 +0800

    for konga test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     Rails.logger.info('*'*100)
     Rails.logger.info(request.headers['X-User-Id'])
+    Rails.logger.info(request.headers['Host'])
   end
 end
